006c9e5ee13bc95cba5b107edf050a5c69da5956
isabella-66 <amasprog@gmail.com>
2022-06-05 17:29:47 -0300

    Criação do diretório 'controller' e do HelloController apropriado

--- crud-spring/src/main/java/com/isabella/controller/HelloController.java
+++ crud-spring/src/main/java/com/isabella/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.isabella;
+package com.isabella.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +10,6 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "Hello, World!";
+        return "Hello, World!!";
     }
 }
