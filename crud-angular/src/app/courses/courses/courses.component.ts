fe5240d3a507520ce72709b5cd61cb7bb882742e
isabella-66 <amasprog@gmail.com>
2022-06-05 10:28:00 -0300

    Adição de tratamento de error, através do catchError, e retorno pop-up, através do MatDialog

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,8 +1,10 @@
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
 import { CoursesService } from './../services/courses.service';
 import { Component, OnInit } from '@angular/core';
 
 import { Course } from '../model/course';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-courses',
@@ -19,15 +21,34 @@ export class CoursesComponent implements OnInit {
   /* coursesService: CoursesService; */
 
   //injeção só quando tem @Injectable
-  constructor(private coursesService: CoursesService) {
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ) {
     /* this.courses = []; //inicializando variável
     /* this.coursesService = new CoursesService(); //instanciação de classe */
 
-    this.courses$ = this.coursesService.list();
+    this.courses$ = this.coursesService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar cursos');
+        return of([])
+      })
+    );
    }
+
+   onError(errorMessage: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMessage
+    });
+  }
 
   ngOnInit(): void {
 
   }
 
 }
+function DialogDataExampleDialog(DialogDataExampleDialog: any, arg1: { data: { animal: string; }; }) {
+  throw new Error('Function not implemented.');
+}
+
