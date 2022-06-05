073560f46013e158779a4383bf9a989a128e12c9
isabella-66 <amasprog@gmail.com>
2022-06-05 17:25:42 -0300

    Correção de erro da criação Spring + Adição de Hello World para teste

--- crud-spring/src/main/java/com/isabella/CrudSpringApplication.java
+++ crud-spring/src/main/java/com/isabella/CrudSpringApplication.java
@@ -1,4 +1,4 @@
-package com.isabella.crudspring;
+package com.isabella;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- crud-spring/src/test/java/com/isabella/CrudSpringApplicationTests.java
+++ crud-spring/src/test/java/com/isabella/CrudSpringApplicationTests.java
@@ -1,4 +1,4 @@
-package com.isabella.crudspring;
+package com.isabella;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
