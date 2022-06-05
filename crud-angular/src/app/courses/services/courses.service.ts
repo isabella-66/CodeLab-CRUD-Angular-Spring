9dbb07f8f23af2c1cb6b2e34cfd376e6a1eb5c9d
isabella-66 <amasprog@gmail.com>
2022-06-05 20:49:54 -0300

    Realização da conexão do front-end com o back-end, através de proxy, e do acesso ao Banco de Dados H2

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -10,7 +10,7 @@ import { Course } from '../model/course';
 export class CoursesService {
 
   //caminho p/ chamar no servidor
-  private readonly API = '/assets/courses.json'
+  private readonly API = 'api/courses';
 
   //chamada assíncrona pro servidor - conexão com API
   constructor(private httpClient: HttpClient) { }
