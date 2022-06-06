fe09d3d6cdd4811d048926ac98e94d23bd55550a
isabella-66 <amasprog@gmail.com>
2022-06-05 23:45:58 -0300

    Criação do salvamento efetivo de um curso novo, codificando no back-end e retornando status 201, caso tenha sucesso

--- crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -15,7 +16,8 @@ export class CourseFormComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder,
     private service: CoursesService,
-    private _snackBar: MatSnackBar
+    private _snackBar: MatSnackBar,
+    private location: Location
     ) {
     this.form = this.formBuilder.group({
       name: [null],
@@ -28,11 +30,16 @@ export class CourseFormComponent implements OnInit {
 
   onSubmit() {
     this.service.save(this.form.value)
-        .subscribe(result => console.log(result), error => this.onError());
+        .subscribe(result => this.onSuccess(), error => this.onError());
   }
 
   onCancel() {
-    console.log('onCancel');
+    this.location.back();
+  }
+
+  onSuccess() {
+    this._snackBar.open('Curso salvo com sucesso!', '', { duration: 5000 });
+    this.onCancel();
   }
 
   onError() {
--- crud-spring/src/main/java/com/isabella/controller/CourseController.java
+++ crud-spring/src/main/java/com/isabella/controller/CourseController.java
@@ -2,7 +2,11 @@ package com.isabella.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +27,15 @@ public class CourseController {
         return courseRepository.findAll();
     }
 
+    @PostMapping
+    /* @ResponseStatus(code = HttpStatus.CREATED) */ //caso não realize alterações depois, como no header
+    public ResponseEntity<Course> create(@RequestBody Course course) {
+        /* System.out.println(course.getName()); 
+
+        * usa com @ResponseStatus no início
+        * return courseRepository.save(course); //spring salva entidade no BD, obtém a chave primária, seta no objeto e retorna o objeto com os três campos preenchidos */
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+               .body(courseRepository.save(course));
+    }
 }
